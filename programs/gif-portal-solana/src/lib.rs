0acdd0f71dbdb61eb28334606b7cf300ccea1746
root <root@LAPTOP-IGJ21Q51.localdomain>
2022-01-03 11:04:43 +0530

    test : write solana program 🚨
    - write a basic solana program
    - write a script to test is locally

--- programs/gif-portal-solana/src/lib.rs
+++ programs/gif-portal-solana/src/lib.rs
@@ -1,14 +1,25 @@
+// Its like import statement
+// Importing tools Anchor provides us to make writing Solana programs easier
 use anchor_lang::prelude::*;
 
+// This is the Program Id
+// has info for Solana on how to run our program
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// This is how we tell our program, everything in this little module below is our program
+// this lets us actually call our Solana program from our frontend via a fetch request
+// This are basically called macros, they attach code to our module. It's sorta like "inheriting" a class.
 #[program]
+
+// mod is a rust module, kinda like a class
 pub mod gif_portal_solana {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    // A function start_stuff_off which takes something called a Context and outputs a ProgramResult
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
         Ok(())
     }
 }
 
+// Another macro
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
--- tests/gif-portal-solana.js
+++ tests/gif-portal-solana.js
@@ -1,14 +1,26 @@
 const anchor = require('@project-serum/anchor');
 
-describe('gif-portal-solana', () => {
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  // Configure the client to use the local cluster.
+  // Tell anchor to set provider
   anchor.setProvider(anchor.Provider.env());
+  // Compiles our code in lib.rs and get it deployed locally
+  const program = anchor.workspace.GifPortalSolana;
+  // Call function, we await it which will wait for our local validator to "mine"
+  const tx = await program.rpc.startStuffOff();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.GifPortalSolana;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
